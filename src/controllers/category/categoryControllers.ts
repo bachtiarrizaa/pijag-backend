d1c28a51fa30b5ee1fdf30d97a48a675928b49b5
bachtiarrizaa <bachtiarriza87@gmail.com>
2025-11-20 22:37:50 +0700

    category service & controller

---
+++ src/controllers/category/categoryControllers.ts
@@ -0,0 +1,85 @@
+import { Request, Response } from "express";
+import { sendSuccess, sendError } from "../../utils/respon.handler";
+import { createCategoryServices, getAllCategoriesServices, getCategoryByIdServices, updateCategoryServices } from "../../services/category/categoryServices";
+
+export const createCategoryControllers = async (req: Request, res: Response) => {
+  try {
+    const name = req.body;
+    const createCategory = await createCategoryServices(name);
+    return sendSuccess(
+      res, 201, "Category created successfully",
+      createCategory
+    );
+  } catch (error: any) {
+    console.error("CreateCategory Error:", error.message);
+    return sendError(res, error.statusCode || 400, error.message);
+  }
+}
+
+export const getAllCategoriesControllers = async (req: Request, res: Response) => {
+  try {
+    const allCategories = await getAllCategoriesServices();
+    return sendSuccess(res, 200, "All categories fetched successfully", allCategories);
+  } catch (error:any) {
+    console.error("GetAllCategories error:", error.message);
+    return sendError(res, error.statusCode || 400, error.message);
+  }
+}
+
+export const getCategoryByIdControllers = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    if (!id) {
+      return sendError(
+        res, 400, "Category Id is required"
+      );
+    }
+
+    const categoryId = parseInt(id, 10);
+
+    const getCategoryById = await getCategoryByIdServices(categoryId)
+    return sendSuccess(res, 200, "Category fetched successfully", getCategoryById);
+  } catch (error: any) {
+    console.error("GetCategoryByIdController Error:", error.message);
+    return sendError(res, error.statusCode || 400, error.message);
+  }
+}
+
+export const updatedCategoryControllers = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { name } = req.body;
+
+    if (!id) {
+      return sendError(
+        res, 201, "Category id is required"
+      );
+    }
+
+    const categoryId = parseInt(id, 10);
+    const updatedCategory = await updateCategoryServices(categoryId, req.body);
+    return sendSuccess(res, 200, "Category updated successfully", updatedCategory);
+  } catch (error: any) {
+    console.error("UpdateCategory Error:", error.message);
+    return sendError(res, error.statusCode || 400, error.message);
+  }
+}
+
+export const deleteCategoryController = async(req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      return sendError(
+        res, 400, "Category Id is required"
+      );
+    }
+    const categoryId = parseInt(id, 10)
+    return sendSuccess(
+      res, 200, "Category deleted successfully", categoryId
+    );
+  } catch (error: any) {
+    console.error("DeleteCategory Error:", error.message);
+    return sendError(res, error.statusCode || 400, error.message);
+  }
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRoutes from "./authRoutes";
+import categoryRoutes from "./categoryRoutes";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
+router.use("/category", categoryRoutes);
 
 export default router;
