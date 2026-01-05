38cd7021f9cef53990aa695144f3a9a05fbe841d
bachtiarrizaa <bachtiarriza87@gmail.com>
2026-01-05 09:35:05 +0700

    Reapply "refactor: refactor auth middleware using class"
    This reverts commit e42211bc179261935bab16ff0d311fe1f5641e33.

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { authenticateToken } from "../middleware/auth.middleware";
 import { AuthController } from "../controllers/auth.controller";
+import { AuthMiddleware } from "../middleware/auth.middleware";
 
 const router = Router();
 const authController = new AuthController();
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
-router.post("/logout", authenticateToken, authController.logout);
+router.post("/logout", AuthMiddleware.isCustomer, authController.logout);
 export default router;
