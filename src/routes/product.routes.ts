5f7257869802d86d02b3c43fc4c7790c5135a8ba
bachtiarrizaa <bachtiarriza87@gmail.com>
2026-01-07 11:03:09 +0700

    refactor: change style upload img middleware using class

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,14 +1,23 @@
 import { Router } from "express";
-import { uploadImgProduct } from "../middleware/uploadImg.middleware";
+// import { uploadImgProduct } from "../middleware/uploadImg.middleware";
 import { AuthMiddleware } from "../middleware/auth.middleware";
 import { ProductController } from "../controllers/product.controller";
+import { UploadImgMiddleware } from "../middleware/upload.middleware";
 
 const router = Router();
 
-router.post("/create", AuthMiddleware.isAdmin, uploadImgProduct, ProductController.create);
+router.post("/create",
+  AuthMiddleware.isAdmin,
+  UploadImgMiddleware.uploadImgProduct,
+  ProductController.create
+);
 router.get("/", ProductController.getProducts)
 router.get("/:id", ProductController.getProductById);
-router.put("/:id", AuthMiddleware.isAdmin, uploadImgProduct, ProductController.update);
+router.put("/:id",
+  AuthMiddleware.isAdmin,
+  UploadImgMiddleware.uploadImgProduct,
+  ProductController.update
+);
 router.delete("/:id", AuthMiddleware.isAdmin, ProductController.delete);
 
 export default router;
--- src/routes/profile.routes.ts
+++ src/routes/profile.routes.ts
@@ -1,11 +1,18 @@
 import { Router } from "express";
-import { uploadAvatar } from "../middleware/uploadImg.middleware";
 import { AuthMiddleware } from "../middleware/auth.middleware";
 import { ProfileController } from "../controllers/profile.controller";
+import { UploadImgMiddleware } from "../middleware/upload.middleware";
 
 const router = Router();
 
-router.get("/", AuthMiddleware.authenticateToken, ProfileController.getProfile);
-router.put("/update", AuthMiddleware.authenticateToken, uploadAvatar, ProfileController.update);
+router.get("/",
+  AuthMiddleware.authenticateToken,
+  ProfileController.getProfile
+);
+router.put("/update",
+  AuthMiddleware.authenticateToken,
+  UploadImgMiddleware.uploadAvatar,
+  ProfileController.update
+);
 
 export default router;
