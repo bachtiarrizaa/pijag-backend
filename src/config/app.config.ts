c0b61a5fd26bc580c646e786e1a5ecd6016775fa
bachtiar <bachtiarriza87@gmail.com>
2026-01-13 16:37:02 +0700

    fix server.ts

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,6 +1,7 @@
 import { AppConfig } from "../types/conifg";
 
 export const appConfig: AppConfig = {
+  APP_HOST: process.env.APP_HOST || "127.0.0.1",
   APP_PORT: parseInt(process.env.APP_PORT || "3000", 10),
   APP_ENV: process.env.APP_ENV || "development",
   JWT_SECRET: process.env.JWT_SECRET || "secret",
